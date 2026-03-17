2fa728a63d05840f4bf058c209f8fc594cfef0bc
Lumic <3474889077@qq.com>
2026-03-17 18:19:57 +0800

    Update 1_Conditional.v

--- Verilog_Codes/Verilog_Language/More_Verilog_Features/1_Conditional.v
+++ Verilog_Codes/Verilog_Language/More_Verilog_Features/1_Conditional.v
@@ -1,1 +1,12 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);
+    wire [7:0] int_result1;
+    wire [7:0] int_result2;
+    assign int_result1 = a<b?a:(b<a?b:a);
+    assign int_result2 = c<d?c:(d<c?d:c);
+    assign min = int_result1<int_result2?
+    	int_result1:(int_result2<int_result1)?
+        int_result2:int_result1;
+endmodule
 
