96de721ba2463f4c192254404b1a0ad1e9eeae74
Lumic <3474889077@qq.com>
2026-03-20 14:44:48 +0800

    Create 9_DFF.v

---
+++ Verilog_Codes/Circuits/Sequential_Logic/Latches_and_Flip_Flops/9_DFF.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,
+    input d, 
+    input r,   // asynchronous reset
+    output reg  q);
+    always @(posedge clk) begin
+        if (r) q <= 0;
+        else q <= d;
+    end
+endmodule
