feb74ec573ed872d1a1704583068b1b368b89ef3
Lumic <3474889077@qq.com>
2026-03-27 22:00:05 +0800

    Update 3_helandun.v

---
+++ Verilog_Codes/Circuits/Sequential_Logic/More_Circuits/3_helandun.v
@@ -0,0 +1,79 @@
+/*module top_module(
+    input clk,
+    input load,
+    input [255:0] data,
+    output reg [255:0] q ); 
+    always @(posedge clk) begin
+        if (load) q <= data;
+        else 
+        	for (int y=0;y<16;y=y+1) begin
+            	for (int x=0;x<16;x=x+1) begin
+                	int num = 0;
+                	// 查询周围细胞
+                    if (q[{(y+1)[3:0],x[3:0]    }] == 1'b1) num=num+1;
+                    if (q[{(y+1)[3:0],(x-1)[3:0]}] == 1'b1) num=num+1;
+                	if (q[{(y+1)[3:0],(x+1)[3:0]}] == 1'b1) num=num+1;
+                    if (q[{y[3:0]    ,(x-1)     }] == 1'b1) num=num+1;
+                    if (q[{y[3:0]    ,(x-1)[3:0]}] == 1'b1) num=num+1;
+                    if (q[{(y-1)[3:0],x[3:0]    }] == 1'b1) num=num+1;
+                	if (q[{(y-1)[3:0],(x-1)[3:0]}] == 1'b1) num=num+1;
+                	if (q[{(y-1)[3:0],(x+1)[3:0]}] == 1'b1) num=num+1;
+                	//更新细胞
+                    if (num<2) q[{y[3:0],x[3:0]}] <= 1'b0;
+                	else if (num==2) q[{y[3:0],x[3:0]}] <= q[{y[3:0],x[3:0]}];
+                	else if (num==3) q[{y[3:0],x[3:0]}] <= 1'b1;
+                	else q[{y[3:0],x[3:0]}] <= 1'b0;
+            	end
+        	end
+    end
+endmodule*/
+module top_module(
+    input clk,
+    input load,
+    input [255:0] data,
+    output reg [255:0] q
+); 
+    always @(posedge clk) begin
+        if (load) 
+            q <= data;
+        else begin
+            // 使用中间变量存储下一状态
+            reg [255:0] q_next;
+            q_next = q;  // 初始化为当前值
+            
+            for (int y = 0; y < 16; y = y + 1) begin
+                for (int x = 0; x < 16; x = x + 1) begin
+                    int num = 0;
+                    int y_up, y_down, x_left, x_right;
+                    
+                    // 计算环绕的索引
+                    y_up = (y == 0) ? 15 : y - 1;
+                    y_down = (y == 15) ? 0 : y + 1;
+                    x_left = (x == 0) ? 15 : x - 1;
+                    x_right = (x == 15) ? 0 : x + 1;
+                    
+                    // 查询周围8个细胞
+                    if (q[y_down*16 + x_right]) num = num + 1;
+                    if (q[y_down*16 + x])     num = num + 1;
+                    if (q[y_down*16 + x_left]) num = num + 1;
+                    if (q[y*16 + x_right])     num = num + 1;
+                    if (q[y*16 + x_left])      num = num + 1;
+                    if (q[y_up*16 + x_right])  num = num + 1;
+                    if (q[y_up*16 + x])        num = num + 1;
+                    if (q[y_up*16 + x_left])   num = num + 1;
+                    
+                    // 更新细胞状态
+                    if (num < 2)
+                        q_next[y*16 + x] = 1'b0;
+                    else if (num == 2)
+                        q_next[y*16 + x] = q[y*16 + x];
+                    else if (num == 3)
+                        q_next[y*16 + x] = 1'b1;
+                    else
+                        q_next[y*16 + x] = 1'b0;
+                end
+            end
+            q <= q_next;
+        end
+    end
+endmodule
